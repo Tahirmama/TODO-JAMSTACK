0ba9c1a35e2496b7c129d258ec922997e07d2bc8
TAHIR <tahirmama8@gmail.com>
2021-10-05 13:28:40 +0500

    Adding Theme ui
    Adding theme-ui library with some heading and button in index.js

--- packages/app/src/pages/index.js
+++ packages/app/src/pages/index.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { Container, Heading, Button, Flex  } from "@theme-ui/components";
 export default props => (
-  <div>
-    <h1>Our Site</h1>
-  </div>
+  <Container>
+    <Flex sx={{flexDirection: "column", padding:4}}>
+    <Heading as="h1">
+      TODO APP
+    </Heading>
+    <Button sx={{
+      marginTop: 3,
+      '&:hover': {
+        backgroundColor: 'White',
+        color: 'primary',
+        cursor: 'pointer'
+      }
+     
+    }
+  }
+  onClick={()=> {
+    alert("clicked")
+  }}  >
+      LOG IN
+    </Button>
+    </Flex>
+  </Container>
 );

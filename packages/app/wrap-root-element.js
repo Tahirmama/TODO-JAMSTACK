595a12694f5057a431b3efb494de9d81e239a79c
TAHIR <tahirmama8@gmail.com>
2021-10-07 20:25:00 +0500

    removing functions

--- packages/app/wrap-root-element.js
+++ packages/app/wrap-root-element.js
@@ -3,7 +3,6 @@ const { ThemeProvider } = require('theme-ui');
 const {dark} = require('@theme-ui/presets');
 const { Provider } = require("./identity-context");
 
-
 const newTheme = {
   ...dark,
   sizes: { container: 1024 }
